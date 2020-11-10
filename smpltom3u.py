ca0aecf5cf84d59f797a1e2512b65c2f87feb5bb
hjs0410hc <35911602+hjs0410hc@users.noreply.github.com>
2020-11-10 21:02:42 +0900

    core file added

---
+++ smpltom3u.py
@@ -0,0 +1,17 @@
+import json
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='smpl to m3u')
+parser.add_argument('filename',type=str,help='converting target(.smpl)')
+args = parser.parse_args()
+filename = args.filename
+destination = os.path.splitext(filename)[0] + ".m3u8"
+with open(filename,"r",encoding="utf-8") as json_file:
+    jsstr = json.load(json_file)
+with open(destination,"w",encoding="utf-8") as dest_file:
+    dest_file.write("#EXTM3U\n")
+    for i in jsstr["members"]:
+        newstr = i["info"].replace("/storage/emulated/0/","D:\\").replace("/","\\")
+        newstr += "\n"
+        dest_file.write(newstr)
